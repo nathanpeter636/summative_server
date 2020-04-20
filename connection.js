21b67bce1393bfe3fe19dd49e0ba4594d471b8a8
SEND-IT <you@example.com>
2020-04-21 11:43:03 +1200

    updating server

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
-let dbname = "books_and_things";
-let username = "kjdone";
+let dbname = "Freebies";
+let username = "root";
 let password = "root";
 
-exports.atlas = `mongodb+srv://${username}:${password}@yoobee2020-vhp1l.gcp.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+exports.atlas = `mongodb://${username}:${password}@cluster0-shard-00-00-bbbaq.gcp.mongodb.net:27017,cluster0-shard-00-01-bbbaq.gcp.mongodb.net:27017,cluster0-shard-00-02-bbbaq.gcp.mongodb.net:27017/${dbname}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`
